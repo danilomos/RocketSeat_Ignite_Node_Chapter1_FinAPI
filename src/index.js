5120dcbad74b5cf1c338ac7d342e17300f6b241d
Danilo Moreira da Silva <xliudanilox@gmail.com>
2021-10-04 14:28:50 -0300

    feat: create account

--- src/index.js
+++ src/index.js
@@ -1,38 +1,21 @@
 const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 const PORT = 3333;
 
 const app = express();
 
+const customers = [];
+
 app.use(express.json())
 
-app.get("/courses", (req, res) => {
-    const query = req.query;
-    console.log(query);
-    return res.json(["Curso 1", "Curso 2", "Curso 3"]);
-});
+app.post("/account", (req, res) => {
+    const { cpf, name } = req.body;
 
-app.post("/courses", (req, res) => {
-    const body = req.body;
-    console.log(body);
-    return res.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
-});
+    const id = uuidv4();
 
-app.put("/courses/:id", (req, res) => {
-    const { id } = req.params;
-    console.log(id);
-    return res.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
-});
-
-app.patch("/courses/:id", (req, res) => {
-    const { id } = req.params;
-    console.log(id);
-    return res.json(["Curso 6", "Curso 7", "Curso 3", "Curso 4"]);
-});
-
-app.delete("/courses/:id", (req, res) => {
-    const { id } = req.params;
-    console.log(id);
-    return res.json(["Curso 6", "Curso 7", "Curso 4"]);
+    customers.push({ id, cpf, name, statements: [] });
+    console.log(customers);
+    return res.status(201).send();
 });
 
 app.listen(PORT);
